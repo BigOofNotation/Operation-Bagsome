2866accb729d7b324b345d67c55992fa9de30950
josuedelatorre35 <josuedelatorre35@yahoo.com>
2024-10-07 19:30:49 -0700

    Initializing BagDriver

---
+++ BagDriver.java
@@ -0,0 +1,49 @@
+public class BagDriver {
+
+    public static void main(String[] args) {
+
+        ResizableArrayBagDemo();
+        LinkedBagDemo();
+
+    }
+        
+    public static void ResizableArrayBagDemo() {
+
+        //Initializing Bag 1 and Bag 2
+
+        // Printing contents of Bag 1 and Bag 2
+
+        // Demonstrating Union
+
+        // Demonstrating Intersection
+
+        // Demonstrating Difference (Bag 1 to Bag 2)
+
+        // Demonstrating Difference (Bag 2 to Bag 1)
+
+        /* Printing contents of Bags 1 and 2 again, 
+        showing content of both bags did not change */
+
+    }
+
+    public static void LinkedBagDemo() {
+
+        //Initializing Bag 1 and Bag 2
+
+        // Printing contents of Bag 1 and Bag 2
+
+        // Demonstrating Union
+
+        // Demonstrating Intersection
+
+        // Demonstrating Difference (Bag 1 to Bag 2)
+
+        // Demonstrating Difference (Bag 2 to Bag 1)
+
+        /* Printing contents of Bags 1 and 2 again, 
+        showing contents of both bags did not change */
+
+    }
+
+
+}
